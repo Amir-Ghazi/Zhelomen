5b69678de60b2a35c63ffc6c0abf53bc9dbf5bc2
Amir-Ghazi <166246744+Amir-Ghazi@users.noreply.github.com>
2024-05-24 12:00:10 +0330

    Add files via upload

---
+++ script/scrool.js
@@ -0,0 +1,21 @@
+document.body, addEventListener("load", function () {
+    this.window.scrollTo(0,700);
+})
+
+const button = document.getElementById('about');
+const articleButton = document.getElementById("arBtn")
+const fileURL = '../index.html';
+
+button.addEventListener('click', function () {
+    const newWindow = window.open(fileURL);
+    newWindow.addEventListener('load', function () {
+        this.window.scrollTo(0,1300)
+    });
+});
+
+articleButton.addEventListener('click', function () {
+    const newWindow = window.open(fileURL);
+    newWindow.addEventListener('load', function () {
+        this.window.scrollTo(0,700)
+    });
+});

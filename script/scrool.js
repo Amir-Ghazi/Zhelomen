60b296c7826cf1d9ed971b04ed265585a1a4c6e3
Amir-Ghazi <166246744+Amir-Ghazi@users.noreply.github.com>
2024-05-24 13:10:20 +0330

    Update scrool.js

--- script/scrool.js
+++ script/scrool.js
@@ -4,18 +4,18 @@ document.body, addEventListener("load", function () {
 
 const button = document.getElementById('about');
 const articleButton = document.getElementById("arBtn")
-const fileURL = 'https://[Amir-Ghazi.github.io/Zhelomen-/index.html';
+const fileURL = 'https://Amir-Ghazi.github.io/Zhelomen-/index.html';
 
 button.addEventListener('click', function () {
     const newWindow = window.open(fileURL);
     newWindow.addEventListener('load', function () {
-        this.window.scrollTo(0,1300)
+        this.window.scrollTo(0,1300);
     });
 });
 
 articleButton.addEventListener('click', function () {
     const newWindow = window.open(fileURL);
     newWindow.addEventListener('load', function () {
-        this.window.scrollTo(0,700)
+        this.window.scrollTo(0,700);
     });
 });

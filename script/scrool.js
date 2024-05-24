f7c92e20d9d2e31a9fa0990d02d683c71238b149
Amir-Ghazi <166246744+Amir-Ghazi@users.noreply.github.com>
2024-05-24 20:12:58 +0330

    Update scrool.js

--- script/scrool.js
+++ script/scrool.js
@@ -2,17 +2,25 @@ document.body, addEventListener("load", function () {
     this.window.scrollTo(0,700);
 })
 
+const message = document.querySelector(".container")
 const articleButton = document.getElementById("arBtn")
 const button = document.getElementById('about');
 
 button.addEventListener('click', function () {
-  newWindow.addEventListener('load', function () {
-    this.window.scrollTo(0, 1300);
-  });
+    animation()
 });
-
 articleButton.addEventListener('click', function () {
-  newWindow.addEventListener('load', function () {
-    this.window.scrollTo(0, 700);
-    });
-});
+    animation()
+})
+function animation() {
+    document.documentElement.scrollTop = 0;
+    message.style.display = "inline-block"
+    let x = 0
+    let timer = setInterval(() => {
+        x++
+        if (x > 3) {
+            message.style.display = "none"
+            clearInterval(timer)
+        }
+    }, 1000);
+}
